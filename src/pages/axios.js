6897d3ab75cff55ead6b26765f7adb296243c16d
Shreeya Pankaj Gaikwad <Shreeya.reena@gmail.com>
2023-10-16 22:14:44 +0530

    axios new 1

--- src/pages/axios.js
+++ src/pages/axios.js
@@ -1,11 +1,9 @@
-// axiosConfig.js
-
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://mezz-back.onrender.com/", // Replace with the actual URL of your backend
+  baseURL: 'https://mezz-back.onrender.com', // Replace with your backend URL
 
+  withCredentials: true, // This is important to allow cookies to be sent with the request
 });
 
 export default axiosInstance;
-
